7d705ef59d2b2ea5dc8fabaf7e11d1ce64ecb97c
jesiica <jeda_221@hotmail.com>
2014-11-04 14:48:00 -0600

    funcionalidad pestania venue

--- app/controllers/Venue.js
+++ app/controllers/Venue.js
@@ -1,1 +1,9 @@
 var args = arguments[0] || {};
+
+$.direccion.addEventListener('click', function(_e) {
+    Titanium.Platform.openURL("https://www.google.com/maps/place/14%C2%B032'49.0%22N+121%C2%B000'57.2%22E/@14.5469581,121.015897,19z/data=!4m2!3m1!1s0x0:0x0");
+});
+
+$.telefono.addEventListener('click', function(e) {
+    Titanium.Platform.openURL("tel:+44 1706 815678");
+});
